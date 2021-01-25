38ad02bb54dd35148ec40ae61f981a9ac87b427d
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-01-25 16:45:10 +0300

    Summary

--- LoadFilesApp/views.py
+++ LoadFilesApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here:
+def load_files(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = DocumentForm()
+    return render(request, 'loadfiles.html', {
+        'form': form
+    })
--- LoadFilesProject/urls.py
+++ LoadFilesProject/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from LoadFilesApp.views import load_files
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('loadfiles/', load_files),
 ]
