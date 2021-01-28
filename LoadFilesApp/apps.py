1785d2060b150e2dd3e372bf6e48d7a1d7c352ea
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-01-28 15:27:33 +0300

    Summary

--- LoadFilesApp/apps.py
+++ LoadFilesApp/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class LoadfilesappConfig(AppConfig):
     name = 'LoadFilesApp'
+    verbose_name = 'Приложение "Книги"'
--- LoadFilesApp/models.py
+++ LoadFilesApp/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 # Create your models here:
 class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    pdf = models.FileField(upload_to='books/pdf/')
-    cover = models.ImageField(upload_to='books/covers/', null=True, blank=True)
+    title = models.CharField(max_length=100, verbose_name="Название")
+    author = models.CharField(max_length=100, verbose_name="Автор")
+    pdf = models.FileField(upload_to='books/pdf/', verbose_name="PDF-документ")
+    cover = models.ImageField(upload_to='books/covers/', null=True, blank=True, verbose_name="Обложка")
+    
+    class Meta:
+        verbose_name = "Название книги"
+        verbose_name_plural = "Названия книг"
     
     def __str__(self):
         return self.title
